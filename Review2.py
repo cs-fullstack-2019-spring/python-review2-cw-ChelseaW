6996679659e12fc1cb58a884b61efb729d0a9a6f
ChelseaW <c.winfrey93@yahoo.com>
2019-01-28 16:43:07 -0600

    Nope

---
+++ Review2.py
@@ -0,0 +1,76 @@
+
+# Create a task list. A user is presented with the text below.
+# Let them select an option to list all of their tasks, add a task to their list, delete a task, or quit the program.
+# Make each option a different function in your program.
+# Do NOT use Google.
+# Do NOT use other students.
+# Try to do this on your own.
+
+
+def main():
+    problem1()
+
+def problem1():
+
+    user = ""
+    thingsToDo = ["dishes", "cut grass", "cook"]
+    while thingsToDo != "0":
+        thingsToDo = input(" Enter a task:")
+
+        print(Task1 = "List all task", Task2 = "Add a task to the list", Task3 = "Delete a task", Task4 = "To quit the program:")
+
+
+#     myList:
+# [
+#
+# ]
+
+# user = input(thingsToDo)
+
+
+
+# def dishes():
+#     print(thingsToDo)
+#
+# def cut grass():
+#
+# def cook():
+
+
+
+
+
+
+
+    # user = input("No")
+# if thingsToDo !=:
+
+
+
+# forEach in thingsToDo
+# thingsToDo.append("car wash")
+# print(thingsToDo)
+# thingsToDo.remove("dishes")
+
+
+# user = input("Congratulations! You're running [YOUR NAME]'s Task List program.What would you like to do next?
+#     1. List all tasks.
+#     2. Add a task to the list.
+#     3. Delete a task.
+#     0. To quit the program")
+
+
+# def dishes():
+#     # while(True):
+#     #     if("dishes" != thingsToDo):
+#     #     print("car wash")
+#
+# def cut grass();
+#     # while():
+#
+# def cook();
+
+
+
+if __name__ == '__main__':
+    main()
